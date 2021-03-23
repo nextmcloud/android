54f307748b4bad483229f6eeba6af9b0b3cb159a
A117870935 <surinder.kumar@t-systems.com>
2021-03-23 18:43:16 +0530

    Scan implementation using Scanbot, test cases added and Scan Event tracking. Scanbot migration 1.89.0 -> 4.0.0 NMC-3671 -- e2ee folder selection if e2ee is already setup NMC-3670 -- crash fix while saving into e2ee folder without e2ee setup NMC-3699 & NMC-3701 -- fix e2ee folder path NMC-3702 -- create sub-folder and open them properly for e2ee folders NMC-3966 -- Scanbot migration 4.0.0 to 6.2.0 NMC-4079 -- fix android 15 keyboard overlaps on password input field. NMC-3512 -- show correct scan path in toolbar after scanning NMC-4725 -- migrate to 7.1.1 from 6.2.0 NMC-4766 -- fix updating scan path after successful folder creation

--- app/src/main/java/com/owncloud/android/ui/adapter/helper/OCFileListAdapterHelper.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/helper/OCFileListAdapterHelper.kt
@@ -39,6 +39,8 @@ class OCFileListAdapterHelper {
         dataProvider: OCFileListAdapterDataProvider,
         onlyOnDevice: Boolean,
         limitToMimeType: String,
+        showOnlyFolder: Boolean,
+        hideEncryptedFolder: Boolean,
         preferences: AppPreferences,
         userId: String,
         onComplete: (List<OCFile>, FileSortOrder) -> Unit
@@ -52,6 +54,8 @@ class OCFileListAdapterHelper {
                 dataProvider,
                 onlyOnDevice,
                 limitToMimeType,
+                showOnlyFolder,
+                hideEncryptedFolder,
                 preferences,
                 userId
             )
@@ -98,6 +102,8 @@ class OCFileListAdapterHelper {
         dataProvider: OCFileListAdapterDataProvider,
         onlyOnDevice: Boolean,
         limitToMimeType: String,
+        showOnlyFolder: Boolean,
+        hideEncryptedFolder: Boolean,
         preferences: AppPreferences,
         userId: String
     ): Pair<List<OCFile>, FileSortOrder> {
@@ -109,6 +115,11 @@ class OCFileListAdapterHelper {
         val filtered = ArrayList<OCFile>(rawResult.size)
 
         for (file in rawResult) {
+            // NMC filter condition to show only folder with or without encrypted folders
+            if ((showOnlyFolder && !file.isFolder) || (hideEncryptedFolder && file.isEncrypted)) {
+                continue
+            }
+
             if (!showHiddenFiles && file.isHidden) {
                 continue
             }
