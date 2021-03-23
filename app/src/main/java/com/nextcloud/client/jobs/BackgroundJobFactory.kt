c5171b4a427ff0bc3fadb7280f781a173dd402ec
A117870935 <surinder.kumar@t-systems.com>
2021-03-23 18:43:16 +0530

    Scan implementation using Scanbot, test cases added and Scan Event tracking. Scanbot migration 1.89.0 -> 4.0.0 NMC-3671 -- e2ee folder selection if e2ee is already setup NMC-3670 -- crash fix while saving into e2ee folder without e2ee setup NMC-3699 & NMC-3701 -- fix e2ee folder path NMC-3702 -- create sub-folder and open them properly for e2ee folders NMC-3966 -- Scanbot migration 4.0.0 to 6.2.0 NMC-4079 -- fix android 15 keyboard overlaps on password input field. NMC-3512 -- show correct scan path in toolbar after scanning NMC-4725 -- migrate to 7.1.1 from 6.2.0 NMC-4766 -- fix updating scan path after successful folder creation

--- app/src/main/java/com/nextcloud/client/jobs/BackgroundJobFactory.kt
+++ app/src/main/java/com/nextcloud/client/jobs/BackgroundJobFactory.kt
@@ -29,6 +29,7 @@ import com.nextcloud.client.jobs.upload.FileUploadWorker
 import com.nextcloud.client.logger.Logger
 import com.nextcloud.client.network.ConnectivityService
 import com.nextcloud.client.preferences.AppPreferences
+import com.nmc.android.jobs.ScanDocUploadWorker
 import com.owncloud.android.datamodel.ArbitraryDataProvider
 import com.owncloud.android.datamodel.SyncedFolderProvider
 import com.owncloud.android.datamodel.UploadsStorageManager
@@ -95,6 +96,7 @@ class BackgroundJobFactory @Inject constructor(
                 FileUploadWorker::class -> createFilesUploadWorker(context, workerParameters)
                 FileDownloadWorker::class -> createFilesDownloadWorker(context, workerParameters)
                 GeneratePdfFromImagesWork::class -> createPDFGenerateWork(context, workerParameters)
+                ScanDocUploadWorker::class -> createScanDocUploadWork(context, workerParameters)
                 HealthStatusWork::class -> createHealthStatusWork(context, workerParameters)
                 TestJob::class -> createTestJob(context, workerParameters)
                 OfflineOperationsWorker::class -> createOfflineOperationsWorker(context, workerParameters)
@@ -255,6 +257,15 @@ class BackgroundJobFactory @Inject constructor(
             logger = logger,
             params = params
         )
+
+    private fun createScanDocUploadWork(context: Context, params: WorkerParameters): ScanDocUploadWorker {
+        return ScanDocUploadWorker(
+            context = context,
+            params = params,
+            notificationManager,
+            accountManager
+        )
+    }
 
     private fun createHealthStatusWork(context: Context, params: WorkerParameters): HealthStatusWork = HealthStatusWork(
         context,
