554079f74f82faa49a6ca15327315843f2992aac
A117870935 <surinder.kumar@t-systems.com>
2021-03-23 18:43:16 +0530

    Scan implementation using Scanbot, test cases added and Scan Event tracking. Scanbot migration 1.89.0 -> 4.0.0 NMC-3671 -- e2ee folder selection if e2ee is already setup NMC-3670 -- crash fix while saving into e2ee folder without e2ee setup NMC-3699 & NMC-3701 -- fix e2ee folder path NMC-3702 -- create sub-folder and open them properly for e2ee folders NMC-3966 -- Scanbot migration 4.0.0 to 6.2.0 NMC-4079 -- fix android 15 keyboard overlaps on password input field. NMC-3512 -- show correct scan path in toolbar after scanning NMC-4725 -- migrate to 7.1.1 from 6.2.0 NMC-4766 -- fix updating scan path after successful folder creation

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListBottomSheetDialog.kt
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListBottomSheetDialog.kt
@@ -28,6 +28,7 @@ import com.owncloud.android.ui.activity.FileActivity
 import com.owncloud.android.utils.MimeTypeUtil
 import com.owncloud.android.utils.theme.ThemeUtils
 import com.owncloud.android.utils.theme.ViewThemeUtils
+import com.nmc.android.utils.ScanBotSdkUtils
 
 @Suppress("LongParameterList")
 class OCFileListBottomSheetDialog(
@@ -61,6 +62,13 @@ class OCFileListBottomSheetDialog(
 
         if (!deviceInfo.hasCamera(context)) {
             binding.menuDirectCameraUpload.visibility = View.GONE
+            binding.menuScanDocument.visibility = View.GONE
+        }
+
+        // check if scanbot sdk licence is valid or not
+        // hide the view if license is not valid
+        if (!ScanBotSdkUtils.isScanBotLicenseValid(fileActivity)) {
+            binding.menuScanDocument.visibility = View.GONE;
         }
 
         createRichWorkspace()
@@ -205,6 +213,11 @@ class OCFileListBottomSheetDialog(
 
             menuNewPresentation.setOnClickListener {
                 actions.newPresentation()
+                dismiss()
+            }
+
+            menuScanDocument.setOnClickListener{
+                actions.scanDocument()
                 dismiss()
             }
         }
