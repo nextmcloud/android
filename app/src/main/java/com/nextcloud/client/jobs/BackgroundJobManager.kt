7c4e5be8b79af68e56bf0242bf8930e33aef3c7b
A117870935 <surinder.kumar@t-systems.com>
2021-03-23 18:43:16 +0530

    Scan implementation using Scanbot, test cases added and Scan Event tracking. Scanbot migration 1.89.0 -> 4.0.0 NMC-3671 -- e2ee folder selection if e2ee is already setup NMC-3670 -- crash fix while saving into e2ee folder without e2ee setup NMC-3699 & NMC-3701 -- fix e2ee folder path NMC-3702 -- create sub-folder and open them properly for e2ee folders NMC-3966 -- Scanbot migration 4.0.0 to 6.2.0 NMC-4079 -- fix android 15 keyboard overlaps on password input field. NMC-3512 -- show correct scan path in toolbar after scanning NMC-4725 -- migrate to 7.1.1 from 6.2.0 NMC-4766 -- fix updating scan path after successful folder creation

--- app/src/main/java/com/nextcloud/client/jobs/BackgroundJobManager.kt
+++ app/src/main/java/com/nextcloud/client/jobs/BackgroundJobManager.kt
@@ -151,6 +151,13 @@ interface BackgroundJobManager {
 
     fun startPdfGenerateAndUploadWork(user: User, uploadFolder: String, imagePaths: List<String>, pdfPath: String)
 
+    fun scheduleImmediateScanDocUploadJob(
+        saveFileTypes: String,
+        docFileName: String,
+        remotePathToUpload: String,
+        pdfPassword: String?
+    ): LiveData<JobInfo?>
+
     fun scheduleTestJob()
     fun startImmediateTestJob()
     fun cancelTestJob()
@@ -166,4 +173,6 @@ interface BackgroundJobManager {
     fun startMetadataSyncJob(currentDirPath: String)
     fun downloadFolder(folder: OCFile, accountName: String)
     fun cancelFolderDownload()
+
+    fun isWorkScheduled(tag: String): Boolean
 }
