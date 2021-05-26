c729823e757b6abe967d9c64d4318585c5aea455
A117870935 <surinder.kumar@t-systems.com>
2021-05-26 09:53:43 +0530

    Implemented MC code conflict dialog.

--- app/src/main/java/com/nextcloud/client/di/ComponentsModule.java
+++ app/src/main/java/com/nextcloud/client/di/ComponentsModule.java
@@ -30,6 +30,7 @@ import com.nextcloud.ui.ImageDetailFragment;
 import com.nextcloud.ui.SetStatusDialogFragment;
 import com.nextcloud.ui.composeActivity.ComposeActivity;
 import com.nextcloud.ui.fileactions.FileActionsBottomSheet;
+import com.nmc.android.ui.conflict.ConflictsResolveConsentDialog;
 import com.nmc.android.ui.LauncherActivity;
 import com.owncloud.android.MainApp;
 import com.owncloud.android.authentication.AuthenticatorActivity;
@@ -370,6 +371,9 @@ abstract class ComponentsModule {
 
     @ContributesAndroidInjector
     abstract ConflictsResolveDialog conflictsResolveDialog();
+
+    @ContributesAndroidInjector
+    abstract ConflictsResolveConsentDialog conflictsResolveConsentDialog();
 
     @ContributesAndroidInjector
     abstract CreateFolderDialogFragment createFolderDialogFragment();
