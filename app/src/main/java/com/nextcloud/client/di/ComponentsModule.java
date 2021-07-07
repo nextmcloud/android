fd105b6dd996a88089fc80d45cf393c89d1de65e
A117870935 <surinder.kumar@t-systems.com>
2021-07-07 17:21:49 +0530

    Implemented privacy settings

--- app/src/main/java/com/nextcloud/client/di/ComponentsModule.java
+++ app/src/main/java/com/nextcloud/client/di/ComponentsModule.java
@@ -39,6 +39,8 @@ import com.nextcloud.ui.ImageDetailFragment;
 import com.nextcloud.ui.SetStatusDialogFragment;
 import com.nextcloud.ui.fileactions.FileActionsBottomSheet;
 import com.nmc.android.ui.LauncherActivity;
+import com.nmc.android.ui.LoginPrivacySettingsActivity;
+import com.nmc.android.ui.PrivacySettingsActivity;
 import com.owncloud.android.MainApp;
 import com.owncloud.android.authentication.AuthenticatorActivity;
 import com.owncloud.android.authentication.DeepLinkLoginActivity;
@@ -466,6 +468,12 @@ abstract class ComponentsModule {
 
     @ContributesAndroidInjector
     abstract DocumentScanActivity documentScanActivity();
+
+    @ContributesAndroidInjector
+    abstract PrivacySettingsActivity privacySettingsActivity();
+
+    @ContributesAndroidInjector
+    abstract LoginPrivacySettingsActivity loginPrivacySettingsActivity();
 
     @ContributesAndroidInjector
     abstract GroupfolderListFragment groupfolderListFragment();
