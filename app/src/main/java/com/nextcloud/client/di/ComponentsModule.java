14543a8062c7a24bdabb4956a8d753fbf1f7696d
A117870935 <surinder.kumar@t-systems.com>
2021-07-07 17:21:49 +0530

    Implemented privacy settings NMC-3765: App stuck on launch screen after re-opening it.

--- app/src/main/java/com/nextcloud/client/di/ComponentsModule.java
+++ app/src/main/java/com/nextcloud/client/di/ComponentsModule.java
@@ -35,6 +35,8 @@ import com.nextcloud.ui.composeActivity.ComposeActivity;
 import com.nextcloud.ui.fileactions.FileActionsBottomSheet;
 import com.nextcloud.ui.trashbinFileActions.TrashbinFileActionsBottomSheet;
 import com.nmc.android.ui.LauncherActivity;
+import com.nmc.android.ui.LoginPrivacySettingsActivity;
+import com.nmc.android.ui.PrivacySettingsActivity;
 import com.owncloud.android.MainApp;
 import com.owncloud.android.authentication.AuthenticatorActivity;
 import com.owncloud.android.authentication.DeepLinkLoginActivity;
@@ -473,6 +475,12 @@ abstract class ComponentsModule {
 
     @ContributesAndroidInjector
     abstract DocumentScanActivity documentScanActivity();
+
+    @ContributesAndroidInjector
+    abstract PrivacySettingsActivity privacySettingsActivity();
+
+    @ContributesAndroidInjector
+    abstract LoginPrivacySettingsActivity loginPrivacySettingsActivity();
 
     @ContributesAndroidInjector
     abstract GroupfolderListFragment groupfolderListFragment();
