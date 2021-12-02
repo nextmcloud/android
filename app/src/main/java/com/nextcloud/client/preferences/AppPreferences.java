3572a3d903d4e63545347ece730d6ddd45afeba6
A117870935 <surinder.kumar@t-systems.com>
2021-12-02 20:57:57 +0530

    implemented onboarding

--- app/src/main/java/com/nextcloud/client/preferences/AppPreferences.java
+++ app/src/main/java/com/nextcloud/client/preferences/AppPreferences.java
@@ -377,6 +377,10 @@ public interface AppPreferences {
 
     void setCalendarLastBackup(long timestamp);
 
+    void setOnBoardingComplete(boolean isCompleted);
+
+    boolean getOnBoardingComplete();
+
     void setPdfZoomTipShownCount(int count);
 
     int getPdfZoomTipShownCount();
