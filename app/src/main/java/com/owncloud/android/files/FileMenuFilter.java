652ef9172bbc3ff1bab232a1f2b665354b7f23e6
A117870935 <surinder.kumar@t-systems.com>
2022-12-08 14:13:30 +0530

    Removed Favorite option for e2ee folders and files.

--- app/src/main/java/com/owncloud/android/files/FileMenuFilter.java
+++ app/src/main/java/com/owncloud/android/files/FileMenuFilter.java
@@ -260,7 +260,8 @@ public class FileMenuFilter {
     }
 
     private void filterFavorite(List<Integer> toShow, List<Integer> toHide, boolean synchronizing) {
-        if (files.isEmpty() || synchronizing || allFavorites()) {
+        if (files.isEmpty() || synchronizing || allFavorites()  || containsEncryptedFile()
+            || containsEncryptedFolder()) {
             toHide.add(R.id.action_favorite);
         } else {
             toShow.add(R.id.action_favorite);
@@ -268,7 +269,8 @@ public class FileMenuFilter {
     }
 
     private void filterUnfavorite(List<Integer> toShow, List<Integer> toHide, boolean synchronizing) {
-        if (files.isEmpty() || synchronizing || allNotFavorites()) {
+        if (files.isEmpty() || synchronizing || allNotFavorites()  || containsEncryptedFile()
+            || containsEncryptedFolder()) {
             toHide.add(R.id.action_unset_favorite);
         } else {
             toShow.add(R.id.action_unset_favorite);
