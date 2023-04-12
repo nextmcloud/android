e3d4e7891a005d89ac93034648da765de65e82d7
A200073727 <akshay.chaurasia@t-systems.com>
2023-04-12 15:57:16 +0530

    implemented notification with customization NMC-2250: Notification tray icon customized

--- app/src/main/java/com/owncloud/android/ui/adapter/NotificationListAdapter.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/NotificationListAdapter.kt
@@ -72,7 +72,6 @@ class NotificationListAdapter(
         bindSubject(holder, notification)
         bindMessage(holder, notification)
         bindIcon(holder, notification)
-        colorViewHolder(holder)
         bindButtons(holder, notification)
     }
 
@@ -148,19 +147,6 @@ class NotificationListAdapter(
         if (notification.getIcon().isNullOrEmpty()) return
         itemClick.onBindIcon(holder.binding.icon, notification.getIcon())
     }
-
-    private fun colorViewHolder(holder: NotificationViewHolder) {
-        viewThemeUtils.platform.run {
-            holder.binding.run {
-                colorImageView(icon, ColorRole.ON_SURFACE_VARIANT)
-                colorTextView(subject, ColorRole.ON_SURFACE)
-                colorTextView(message, ColorRole.ON_SURFACE_VARIANT)
-                colorTextView(datetime, ColorRole.ON_SURFACE_VARIANT)
-            }
-        }
-
-        viewThemeUtils.material.colorMaterialButtonContent(holder.binding.dismiss, ColorRole.ON_SURFACE)
-    }
     // endregion
 
     // region Button binding
@@ -194,7 +180,20 @@ class NotificationListAdapter(
                 buildButton(transparent = true, label = fragment.getString(R.string.more), params = params) {
                     showOverflowMenu(it, overflowActions, holder, notification)
                 }
-            viewThemeUtils.material.colorMaterialButtonPrimaryBorderless(moreButton)
+            // NMC Customization
+            moreButton.setBackgroundColor(
+                ResourcesCompat.getColor(
+                    fragment.resources,
+                    android.R.color.transparent,
+                    null
+                )
+            )
+            moreButton.setTextColor(
+                ResourcesCompat.getColor(
+                    fragment.resources,
+                    R.color.shared_with_me_color, null
+                )
+            )
             holder.binding.buttons.addView(moreButton)
         } else {
             for (action in actions) {
@@ -202,9 +201,34 @@ class NotificationListAdapter(
                     onActionClicked(holder, action, notification)
                 }
                 if (action.primary) {
-                    viewThemeUtils.material.colorMaterialButtonPrimaryFilled(button)
+                    // NMC Customization
+                    button.setBackgroundColor(
+                        ResourcesCompat.getColor(
+                            fragment.resources,
+                            R.color.shared_with_me_color, null
+                        )
+                    )
+                    button.setTextColor(
+                        ResourcesCompat.getColor(
+                            fragment.resources,
+                            R.color.white, null
+                        )
+                    )
                 } else {
-                    viewThemeUtils.material.colorMaterialButtonPrimaryBorderless(button)
+                    // NMC Customization
+                    button.setBackgroundColor(
+                        ResourcesCompat.getColor(
+                            fragment.resources,
+                            android.R.color.transparent,
+                            null
+                        )
+                    )
+                    button.setTextColor(
+                        ResourcesCompat.getColor(
+                            fragment.resources,
+                            R.color.shared_with_me_color, null
+                        )
+                    )
                 }
                 holder.binding.buttons.addView(button)
             }
@@ -220,7 +244,19 @@ class NotificationListAdapter(
         val button = buildButton(transparent = false, label = action.label, params = params) {
             onActionClicked(holder, action, notification)
         }
-        viewThemeUtils.material.colorMaterialButtonPrimaryFilled(button)
+        // NMC Customization
+        button.setBackgroundColor(
+            ResourcesCompat.getColor(
+                fragment.resources,
+                R.color.shared_with_me_color, null
+            )
+        )
+        button.setTextColor(
+            ResourcesCompat.getColor(
+                fragment.resources,
+                R.color.white, null
+            )
+        )
         holder.binding.buttons.addView(button)
     }
 
