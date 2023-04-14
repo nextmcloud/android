234c01158d9d62398c696ddf34b3b5e4093b0ca5
A117870935 <surinder.kumar@t-systems.com>
2023-04-14 16:01:18 +0530

    NMC-1923: E2EE related changes with test cases. NMC-1550: Allow to create encrypted folder directly from bottom sheet dialog from NC PR: https://github.com/nextcloud/android/pull/10782

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -240,6 +240,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
         ADD_GRID_AND_SORT_WITH_SEARCH
     }
 
+    private boolean mShowOnlyFolder, mHideEncryptedFolder;
+
     protected MenuItemAddRemove menuItemAddRemoveValue = MenuItemAddRemove.ADD_GRID_AND_SORT_WITH_SEARCH;
 
     private List<MenuItem> mOriginalMenuItems = new ArrayList<>();
@@ -1480,6 +1482,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
         action.putExtra(FolderPickerActivity.EXTRA_FOLDER, getCurrentFile());
         action.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION); // No animation since we stay in the same folder
         action.putExtra(FolderPickerActivity.EXTRA_ACTION, extraAction);
+        action.putExtra(FolderPickerActivity.EXTRA_SHOW_ONLY_FOLDER, true);
+        action.putExtra(FolderPickerActivity.EXTRA_HIDE_ENCRYPTED_FOLDER, true);
         getActivity().startActivityForResult(action, requestCode);
     }
 
@@ -1499,6 +1503,12 @@ public class OCFileListFragment extends ExtendedListFragment implements
      */
     public void listDirectory(boolean onlyOnDevice) {
         listDirectory(null, onlyOnDevice);
+    }
+
+    public void listDirectoryFolder(@Nullable OCFile directory, boolean onlyOnDevice, boolean showOnlyFolder, boolean hideEncryptedFolder) {
+        mShowOnlyFolder = showOnlyFolder;
+        mHideEncryptedFolder = hideEncryptedFolder;
+        listDirectory(directory, onlyOnDevice);
     }
 
     public void refreshDirectory() {
@@ -1571,7 +1581,9 @@ public class OCFileListFragment extends ExtendedListFragment implements
                 directory,
                 storageManager,
                 onlyOnDevice,
-                mLimitToMimeType);
+                mLimitToMimeType,
+                mShowOnlyFolder,
+                mHideEncryptedFolder);
 
             OCFile previousDirectory = mFile;
             mFile = directory;
