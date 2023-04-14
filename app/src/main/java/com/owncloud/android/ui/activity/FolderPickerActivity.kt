7c2f8a67a1f95788baa97e228395df33904533c8
A117870935 <surinder.kumar@t-systems.com>
2023-04-14 16:01:18 +0530

    E2EE related changes with test cases.

--- app/src/main/java/com/owncloud/android/ui/activity/FolderPickerActivity.kt
+++ app/src/main/java/com/owncloud/android/ui/activity/FolderPickerActivity.kt
@@ -61,6 +61,8 @@ open class FolderPickerActivity :
     private var mSyncBroadcastReceiver: SyncBroadcastReceiver? = null
     private var mSyncInProgress = false
     private var mSearchOnlyFolders = false
+    private var mShowOnlyFolder = false
+    private var mHideEncryptedFolder = false
     var isDoNotEnterEncryptedFolder = false
         private set
 
@@ -114,6 +116,9 @@ open class FolderPickerActivity :
     }
 
     private fun setupAction() {
+        mShowOnlyFolder = intent.getBooleanExtra(EXTRA_SHOW_ONLY_FOLDER, false)
+        mHideEncryptedFolder = intent.getBooleanExtra(EXTRA_HIDE_ENCRYPTED_FOLDER, false)
+
         action = intent.getStringExtra(EXTRA_ACTION)
 
         if (action != null && action == CHOOSE_LOCATION) {
@@ -359,7 +364,7 @@ open class FolderPickerActivity :
         }
 
     private fun refreshListOfFilesFragment(fromSearch: Boolean) {
-        listOfFilesFragment?.listDirectory(false, fromSearch)
+        listOfFilesFragment?.listDirectoryFolder(false, fromSearch, mShowOnlyFolder, mHideEncryptedFolder)
     }
 
     fun browseToRoot() {
@@ -684,6 +689,12 @@ open class FolderPickerActivity :
 
         @JvmField
         val EXTRA_ACTION = FolderPickerActivity::class.java.canonicalName?.plus(".EXTRA_ACTION")
+
+        @JvmField
+        val EXTRA_SHOW_ONLY_FOLDER = FolderPickerActivity::class.java.canonicalName?.plus(".EXTRA_SHOW_ONLY_FOLDER")
+
+        @JvmField
+        val EXTRA_HIDE_ENCRYPTED_FOLDER = FolderPickerActivity::class.java.canonicalName?.plus(".EXTRA_HIDE_ENCRYPTED_FOLDER")
 
         const val MOVE_OR_COPY = "MOVE_OR_COPY"
         const val CHOOSE_LOCATION = "CHOOSE_LOCATION"
