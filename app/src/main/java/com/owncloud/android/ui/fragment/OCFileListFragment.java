06d32baddaf4766ec021f60519e0ecb21396daf3
A117870935 <surinder.kumar@t-systems.com>
2023-04-14 16:01:18 +0530

    NMC-1923: E2EE related changes with test cases. NMC-1550: Allow to create encrypted folder directly from bottom sheet dialog from NC PR: https://github.com/nextcloud/android/pull/10782

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -236,6 +236,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
         ADD_GRID_AND_SORT_WITH_SEARCH
     }
 
+    private boolean mShowOnlyFolder, mHideEncryptedFolder;
+
     protected MenuItemAddRemove menuItemAddRemoveValue = MenuItemAddRemove.ADD_GRID_AND_SORT_WITH_SEARCH;
 
     private final List<MenuItem> mOriginalMenuItems = new ArrayList<>();
@@ -766,7 +768,7 @@ public class OCFileListFragment extends ExtendedListFragment implements
     @Override
     public void showTemplate(Creator creator, String headline) {
         ChooseTemplateDialogFragment.newInstance(mFile, creator, headline).show(requireActivity()
-                                                                                    .getSupportFragmentManager(), 
+                                                                                    .getSupportFragmentManager(),
                                                                                 DIALOG_CREATE_DOCUMENT);
     }
 
@@ -908,8 +910,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
             final int checkedCount = checkedFiles.size();
 
             if (mActiveActionMode != null) {
-                String title = getResources().getQuantityString(R.plurals.items_selected_count, 
-                                                                checkedCount, 
+                String title = getResources().getQuantityString(R.plurals.items_selected_count,
+                                                                checkedCount,
                                                                 checkedCount);
                 mActiveActionMode.setTitle(title);
             }
@@ -1132,8 +1134,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
 
     private void folderOnItemClick(OCFile file, int position) {
         if (requireActivity() instanceof FolderPickerActivity fpa) {
-            String filenameErrorMessage = FileNameValidator.INSTANCE.checkFileName(file.getFileName(), 
-                                                                                   getCapabilities(), 
+            String filenameErrorMessage = FileNameValidator.INSTANCE.checkFileName(file.getFileName(),
+                                                                                   getCapabilities(),
                                                                                    requireContext());
             if (filenameErrorMessage != null) {
                 DisplayUtils.showSnackMessage(fpa, filenameErrorMessage);
@@ -1447,6 +1449,7 @@ public class OCFileListFragment extends ExtendedListFragment implements
         action.putExtra(FolderPickerActivity.EXTRA_FOLDER, getCurrentFile());
         action.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION); // No animation since we stay in the same folder
         action.putExtra(FolderPickerActivity.EXTRA_ACTION, extraAction);
+        action.putExtra(FolderPickerActivity.EXTRA_HIDE_ENCRYPTED_FOLDER, true);
         requireActivity().startActivityForResult(action, requestCode);
     }
 
@@ -1466,6 +1469,12 @@ public class OCFileListFragment extends ExtendedListFragment implements
      */
     public void listDirectory(boolean onlyOnDevice) {
         listDirectory(null, onlyOnDevice);
+    }
+
+    public void listDirectoryFolder(@Nullable OCFile directory, boolean onlyOnDevice, boolean showOnlyFolder, boolean hideEncryptedFolder) {
+        mShowOnlyFolder = showOnlyFolder;
+        mHideEncryptedFolder = hideEncryptedFolder;
+        listDirectory(directory, onlyOnDevice);
     }
 
     public void refreshDirectory() {
@@ -1540,7 +1549,9 @@ public class OCFileListFragment extends ExtendedListFragment implements
                 directory,
                 storageManager,
                 onlyOnDevice,
-                mLimitToMimeType);
+                mLimitToMimeType,
+                mShowOnlyFolder,
+                mHideEncryptedFolder);
 
             OCFile previousDirectory = mFile;
             mFile = directory;
