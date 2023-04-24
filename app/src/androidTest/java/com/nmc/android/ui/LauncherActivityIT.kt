ce2119389ef12daedba772ed0c320070670dff9b
A117870935 <surinder.kumar@t-systems.com>
2023-04-24 11:47:04 +0530

    Login related customization. NMC-3250 -> opening login flow in internal webview instead of external browser. NMC-1885: Splash screen customized NMC-571 -- removed usesCleartextTraffic from Manifest. NMC-3773 -- close app on access denied error comes NMC-3964 -- fix app crash on logout for Xiaomi Android 15 devices NMC-3936 & NMC-3813 -- Enabled edge-to-edge for Api level 35. NMC-4351 -- fix redirection to login screen on timeout NMC-4743 -- fix logout crash from trashbin tab

--- app/src/androidTest/java/com/nmc/android/ui/LauncherActivityIT.kt
+++ app/src/androidTest/java/com/nmc/android/ui/LauncherActivityIT.kt
@@ -6,35 +6,33 @@
  */
 package com.nmc.android.ui
 
-import android.view.View
-import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.owncloud.android.AbstractIT
 import com.owncloud.android.R
-import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LauncherActivityIT : AbstractIT() {
 
-    @Test
-    fun testSplashScreenWithEmptyTitlesShouldHideTitles() {
-        launchActivity<LauncherActivity>().onActivity { activity ->
-            assertEquals(View.VISIBLE, activity.findViewById<View>(R.id.ivSplash).visibility)
-            assertEquals(View.GONE, activity.findViewById<View>(R.id.splashScreenBold).visibility)
-            assertEquals(View.GONE, activity.findViewById<View>(R.id.splashScreenNormal).visibility)
-        }
-    }
+    @get:Rule
+    val activityRule = ActivityScenarioRule(LauncherActivity::class.java)
 
     @Test
-    fun testSplashScreenWithTitlesShouldShowTitles() {
-        launchActivity<LauncherActivity>().onActivity { activity ->
-            activity.setSplashTitles("Example", "Cloud")
+    fun verifyUIElements() {
+        onView(withId(R.id.ivSplash)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.splashScreenBold)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.splashScreenNormal)).check(matches(isCompletelyDisplayed()))
 
-            assertEquals(View.VISIBLE, activity.findViewById<View>(R.id.ivSplash).visibility)
-            assertEquals(View.VISIBLE, activity.findViewById<View>(R.id.splashScreenBold).visibility)
-            assertEquals(View.VISIBLE, activity.findViewById<View>(R.id.splashScreenNormal).visibility)
-        }
+        onView(withId(R.id.splashScreenBold)).check(matches(withText("Magenta")))
+        onView(withId(R.id.splashScreenNormal)).check(matches(withText("CLOUD")))
+        shortSleep()
     }
 }
