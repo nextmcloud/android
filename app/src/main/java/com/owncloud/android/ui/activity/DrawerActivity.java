30dd5aabd753fc6b4d0838310f34673939221909
A117870935 <surinder.kumar@t-systems.com>
2023-04-24 11:47:04 +0530

    Login related customization. NMC-3250 -> opening login flow in internal webview instead of external browser. NMC-1885: Splash screen customized NMC-571 -- removed usesCleartextTraffic from Manifest. NMC-3773 -- close app on access denied error comes NMC-3964 -- fix app crash on logout for Xiaomi Android 15 devices NMC-3936 & NMC-3813 -- Enabled edge-to-edge for Api level 35. NMC-4351 -- fix redirection to login screen on timeout NMC-4743 -- fix logout crash from trashbin tab

--- app/src/main/java/com/owncloud/android/ui/activity/DrawerActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/DrawerActivity.java
@@ -1332,7 +1332,8 @@ public abstract class DrawerActivity extends ToolbarActivity
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onAccountRemovedEvent(AccountRemovedEvent event) {
-        restart();
+        //NMC customization
+        finishAffinity();
     }
 
     /**
