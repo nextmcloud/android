e4656da4befb7f93b7765e27c9a1228035fe31eb
A117870935 <surinder.kumar@t-systems.com>
2023-04-24 11:47:04 +0530

    Login related customization.

--- app/src/main/java/com/owncloud/android/ui/activity/DrawerActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/DrawerActivity.java
@@ -1169,7 +1169,8 @@ public abstract class DrawerActivity extends ToolbarActivity
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onAccountRemovedEvent(AccountRemovedEvent event) {
-        restart();
+        //NMC customization
+        finish();
     }
 
     /**
