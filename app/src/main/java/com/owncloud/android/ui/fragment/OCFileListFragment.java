4b0bd9f0a5e8fcb5df059e43beafff2abdc6497e
A117870935 <surinder.kumar@t-systems.com>
2023-04-28 01:25:43 +0530

    Customizations done for Sharing with test cases. Download limit functionality added. Email selection from contact implemented with Test Cases.

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -94,6 +94,7 @@ import com.owncloud.android.ui.dialog.CreateFolderDialogFragment;
 import com.owncloud.android.ui.dialog.RemoveFilesDialogFragment;
 import com.owncloud.android.ui.dialog.RenameFileDialogFragment;
 import com.owncloud.android.ui.dialog.setupEncryption.SetupEncryptionDialogFragment;
+import com.owncloud.android.ui.dialog.SendShareDialog;
 import com.owncloud.android.ui.dialog.SyncFileNotEnoughSpaceDialogFragment;
 import com.owncloud.android.ui.events.ChangeMenuEvent;
 import com.owncloud.android.ui.events.CommentsEvent;
@@ -601,6 +602,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
 
     @Override
     public void onShareIconClick(OCFile file) {
+        //NMC Customization
+        SendShareDialog.isPeopleShareClicked = true;
         if (file.isFolder()) {
             mContainerActivity.showDetails(file, 1);
         } else {
@@ -1195,6 +1198,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
             OCFile singleFile = checkedFiles.iterator().next();
 
             if (itemId == R.id.action_send_share_file) {
+                //NMC Customization
+                SendShareDialog.isPeopleShareClicked = true;
                 mContainerActivity.getFileOperationsHelper().sendShareFile(singleFile);
                 return true;
             } else if (itemId == R.id.action_open_file_with) {
@@ -1223,7 +1228,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
                 if (mActiveActionMode != null) {
                     mActiveActionMode.finish();
                 }
-
+                //NMC Customization
+                SendShareDialog.isPeopleShareClicked = true;
                 mContainerActivity.showDetails(singleFile);
                 mContainerActivity.showSortListGroup(false);
                 return true;
