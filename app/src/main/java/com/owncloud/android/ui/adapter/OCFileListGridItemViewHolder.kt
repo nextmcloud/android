fea1be7fee08e3f5493c7773927987f1243be8ce
A117870935 <surinder.kumar@t-systems.com>
2023-04-28 01:25:43 +0530

    Customizations done for Sharing with test cases. Download limit functionality added. Email selection from contact implemented with Test Cases. Share new ui updated with divider, index icons, colors, empty state. Received sharing disabled state handled. Test cases updated Updated colors as per NMC-4218 NMC-3928 fix: avoid resetting download counter on every update of advance permission NMC-4653: clear edittext focus on action done

--- app/src/main/java/com/owncloud/android/ui/adapter/OCFileListGridItemViewHolder.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/OCFileListGridItemViewHolder.kt
@@ -51,6 +51,8 @@ class OCFileListGridItemViewHolder(var binding: GridItemBinding) :
         get() = null
     override val shared: ImageView
         get() = binding.sharedIcon
+    override val sharedMessage: TextView?
+        get() = null
     override val checkbox: ImageView
         get() = binding.customCheckbox
     override val itemLayout: View
