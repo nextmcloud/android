1d6602ed2d427e7f3f2f893ef9acc002735983cc
A117870935 <surinder.kumar@t-systems.com>
2023-04-28 01:25:43 +0530

    Share new ui updated with divider, index icons, colors, empty state. Received sharing disabled state handled. Test cases updated

--- app/src/main/java/com/owncloud/android/ui/fragment/FileDetailSharingMenuBottomSheetDialog.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/FileDetailSharingMenuBottomSheetDialog.java
@@ -107,7 +107,6 @@ public class FileDetailSharingMenuBottomSheetDialog extends BottomSheetDialog {
             actions.sendLink(ocShare);
             dismiss();
         });
-
     }
 
     @Override
