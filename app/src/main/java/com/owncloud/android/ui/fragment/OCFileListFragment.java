6c608d335322042b910d11f1ebdc6b16f2af3f1c
A117870935 <surinder.kumar@t-systems.com>
2023-04-28 01:25:43 +0530

    Customizations done for Sharing with test cases. Download limit functionality added. Email selection from contact implemented with Test Cases. Share new ui updated with divider, index icons, colors, empty state. Received sharing disabled state handled. Test cases updated Updated colors as per NMC-4218 NMC-3928 fix: avoid resetting download counter on every update of advance permission NMC-4653: clear edittext focus on action done

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -100,6 +100,7 @@ import com.owncloud.android.ui.dialog.CreateFolderDialogFragment;
 import com.owncloud.android.ui.dialog.RemoveFilesDialogFragment;
 import com.owncloud.android.ui.dialog.RenameFileDialogFragment;
 import com.owncloud.android.ui.dialog.SyncFileNotEnoughSpaceDialogFragment;
+import com.owncloud.android.ui.dialog.SendShareDialog;
 import com.owncloud.android.ui.events.ChangeMenuEvent;
 import com.owncloud.android.ui.events.CommentsEvent;
 import com.owncloud.android.ui.events.EncryptionEvent;
@@ -686,6 +687,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
 
     @Override
     public void onShareIconClick(OCFile file) {
+        //NMC Customization
+        SendShareDialog.isPeopleShareClicked = true;
         mContainerActivity.showDetails(file, 1);
     }
 
@@ -1305,6 +1308,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
             OCFile singleFile = checkedFiles.iterator().next();
 
             if (itemId == R.id.action_send_share_file) {
+                //NMC Customization
+                SendShareDialog.isPeopleShareClicked = true;
                 mContainerActivity.showDetails(singleFile, 1);
                 return true;
             } else if (itemId == R.id.action_open_file_with) {
@@ -1335,7 +1340,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
                 if (mActiveActionMode != null) {
                     mActiveActionMode.finish();
                 }
-
+                //NMC Customization
+                SendShareDialog.isPeopleShareClicked = true;
                 mContainerActivity.showDetails(singleFile);
                 mContainerActivity.showSortListGroup(false);
                 return true;
