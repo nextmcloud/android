80ee8be5e2b767ec61476c90a0779056324cb9ad
A117870935 <surinder.kumar@t-systems.com>
2023-05-10 00:35:12 +0530

    NMC-1920: Customized theming related changes for grid and list views. NMC-2121: Theming applied to passcode. NMC-2374: Image edit functionality customized. NMC-1941: Fixed error message coming after camera permission allowed. NMC-2052: Fix back arrow appearing as hamburger icon. NC PR: https://github.com/nextcloud/android/pull/11769 NMC-2465: Fix local db version to avoid crash during app upgrade. NMC-2576: Disabled switch account SnackBar. NMC-2123: Widget disabled. NMC-2127: System default night mode. NMC-1919: Backup Calendar-Contacts configured with test cases. NMC-2089: Reduce log levels NMC-4633: disable permission check in search screen Media View multi-selection customized NMC-4799: uploads screen icon color fix NMC-4862: deactivate github report snackbar

--- app/src/main/java/com/owncloud/android/ui/adapter/GalleryAdapter.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/GalleryAdapter.kt
@@ -92,7 +92,8 @@ class GalleryAdapter(
             transferServiceGetter,
             showMetadata = false,
             showShareAvatar = false,
-            viewThemeUtils
+            viewThemeUtils,
+            true
         )
     }
 
