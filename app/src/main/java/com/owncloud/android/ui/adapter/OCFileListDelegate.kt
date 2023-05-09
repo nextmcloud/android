738dbf4c8db9400129a33d4b35804acf05da6365
A117870935 <surinder.kumar@t-systems.com>
2023-05-10 00:35:12 +0530

    Customized theming related changes for grid and list views.

--- app/src/main/java/com/owncloud/android/ui/adapter/OCFileListDelegate.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/OCFileListDelegate.kt
@@ -47,11 +47,12 @@ class OCFileListDelegate(
     private val storageManager: FileDataStorageManager,
     private val hideItemOptions: Boolean,
     private val preferences: AppPreferences,
-    private val gridView: Boolean,
+    private var gridView: Boolean,
     private val transferServiceGetter: ComponentsGetter,
     private val showMetadata: Boolean,
     private var showShareAvatar: Boolean,
     private var viewThemeUtils: ViewThemeUtils,
+    private val isMediaGallery: Boolean,
     private val syncFolderProvider: SyncedFolderProvider? = null
 ) {
     private val checkedFiles: MutableSet<OCFile> = HashSet()
@@ -210,7 +211,8 @@ class OCFileListDelegate(
             gridViewHolder.shimmerThumbnail,
             preferences,
             viewThemeUtils,
-            syncFolderProvider
+            syncFolderProvider,
+            isMediaGallery
         )
 
         // item layout + click listeners
@@ -234,7 +236,8 @@ class OCFileListDelegate(
 
         // shares
         val shouldHideShare = (
-            hideItemOptions ||
+            gridView || // NMC: don't show share icon in grid mode
+                hideItemOptions ||
                 !file.isFolder &&
                 file.isEncrypted ||
                 file.isEncrypted &&
@@ -311,9 +314,8 @@ class OCFileListDelegate(
 
     private fun setCheckBoxImage(file: OCFile, gridViewHolder: ListGridImageViewHolder) {
         if (isCheckedFile(file)) {
-            gridViewHolder.checkbox.setImageDrawable(
-                viewThemeUtils.platform.tintDrawable(context, R.drawable.ic_checkbox_marked, ColorRole.PRIMARY)
-            )
+            // NMC Customization
+            gridViewHolder.checkbox.setImageResource(R.drawable.ic_checkbox_marked)
         } else {
             gridViewHolder.checkbox.setImageResource(R.drawable.ic_checkbox_blank_outline)
         }
@@ -399,6 +401,10 @@ class OCFileListDelegate(
 
     fun setShowShareAvatar(bool: Boolean) {
         showShareAvatar = bool
+    }
+
+    fun setGridView(bool: Boolean){
+        gridView = bool
     }
 
     companion object {
