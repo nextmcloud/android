da7908aaf40e98cba72649d4512144f5857f1319
A117870935 <surinder.kumar@t-systems.com>
2023-05-10 00:35:12 +0530

    Customized theming related changes for grid and list views.

--- app/src/main/java/com/owncloud/android/ui/dialog/ConflictsResolveDialog.java
+++ app/src/main/java/com/owncloud/android/ui/dialog/ConflictsResolveDialog.java
@@ -215,7 +215,8 @@ public class ConflictsResolveDialog extends DialogFragment implements Injectable
                                   null,
                                   null,
                                   viewThemeUtils,
-                                  syncedFolderProvider);
+                                  syncedFolderProvider,
+                                  false);
 
         View.OnClickListener checkBoxClickListener = v ->
             positiveButton.setEnabled(binding.newCheckbox.isChecked() || binding.existingCheckbox.isChecked());
