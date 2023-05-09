0bd0f68df11dbebc687f9d7441042f36df4553c4
A117870935 <surinder.kumar@t-systems.com>
2023-05-10 00:35:12 +0530

    NMC-1920: Customized theming related changes for grid and list views. NMC-2121: Theming applied to passcode. NMC-2374: Image edit functionality customized. NMC-1941: Fixed error message coming after camera permission allowed. NMC-2052: Fix back arrow appearing as hamburger icon. NC PR: https://github.com/nextcloud/android/pull/11769 NMC-2465: Fix local db version to avoid crash during app upgrade. NMC-2576: Disabled switch account SnackBar. NMC-2123: Widget disabled. NMC-2127: System default night mode. NMC-1919: Backup Calendar-Contacts configured with test cases. NMC-2089: Reduce log levels NMC-4633: disable permission check in search screen Media View multi-selection customized NMC-4799: uploads screen icon color fix NMC-4862: deactivate github report snackbar
    Added androidTest to verify colors, strings, dimens customized in this branch PR for NMC.

--- app/src/main/java/com/owncloud/android/ui/adapter/OCFileListDelegate.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/OCFileListDelegate.kt
@@ -56,11 +56,12 @@ class OCFileListDelegate(
     private val storageManager: FileDataStorageManager,
     private val hideItemOptions: Boolean,
     private val preferences: AppPreferences,
-    private val gridView: Boolean,
+    private var gridView: Boolean,
     private val transferServiceGetter: ComponentsGetter,
     private val showMetadata: Boolean,
     private var showShareAvatar: Boolean,
     private var viewThemeUtils: ViewThemeUtils,
+    private val isMediaGallery: Boolean,
     private val syncFolderProvider: SyncedFolderProvider? = null
 ) {
     private val tag = "OCFileListDelegate"
@@ -195,7 +196,8 @@ class OCFileListDelegate(
             shimmerThumbnail,
             preferences,
             viewThemeUtils,
-            overlayManager
+            overlayManager,
+            isMediaGallery
         )
     }
 
@@ -230,7 +232,8 @@ class OCFileListDelegate(
 
         // shares
         val shouldHideShare = (
-            hideItemOptions ||
+            gridView || // NMC: don't show share icon in grid mode
+                hideItemOptions ||
                 !file.isFolder &&
                 file.isEncrypted ||
                 file.isEncrypted &&
@@ -311,9 +314,8 @@ class OCFileListDelegate(
 
     private fun setCheckBoxImage(file: OCFile, gridViewHolder: ListViewHolder) {
         if (isCheckedFile(file)) {
-            gridViewHolder.checkbox.setImageDrawable(
-                viewThemeUtils.platform.tintDrawable(context, R.drawable.ic_checkbox_marked, ColorRole.PRIMARY)
-            )
+            // NMC Customization
+            gridViewHolder.checkbox.setImageResource(R.drawable.ic_checkbox_marked)
         } else {
             gridViewHolder.checkbox.setImageResource(R.drawable.ic_checkbox_blank_outline)
         }
@@ -437,6 +439,10 @@ class OCFileListDelegate(
         cancelAllPendingTasks()
 
         Log_OC.d(TAG, "background jobs cancelled")
+    }
+
+    fun setGridView(bool: Boolean){
+        gridView = bool
     }
 
     companion object {
--- app/src/main/java/com/owncloud/android/ui/adapter/UnifiedSearchItemViewHolder.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/UnifiedSearchItemViewHolder.kt
@@ -127,7 +127,9 @@ class UnifiedSearchItemViewHolder(
                 user,
                 preferences,
                 context,
-                viewThemeUtils
+                viewThemeUtils,
+                false,
+                false
             )
         }
     }
