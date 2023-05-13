d91352549572e9defaed321432e1820e7967f449
A117870935 <surinder.kumar@t-systems.com>
2023-05-13 23:30:49 +0530

    Customized dialog theme. NMC-3805 -- logout popup wording adaptation

--- app/src/main/java/com/owncloud/android/ui/fragment/contactsbackup/BackupFragment.kt
+++ app/src/main/java/com/owncloud/android/ui/fragment/contactsbackup/BackupFragment.kt
@@ -391,6 +391,7 @@ class BackupFragment :
         val cal = savedDate ?: Calendar.getInstance()
         datePickerDialog = DatePickerDialog(
             contactsPreferenceActivity,
+            R.style.FallbackDatePickerDialogTheme,
             this,
             cal.get(Calendar.YEAR),
             cal.get(Calendar.MONTH),
