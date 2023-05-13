390b432547222aa3110c0773dfa2bad1d93ef8ec
A117870935 <surinder.kumar@t-systems.com>
2023-05-13 23:30:49 +0530

    Customized dialog theme. NMC-3805 -- logout popup wording adaptation

--- app/src/main/java/com/nextcloud/ui/ChooseStorageLocationDialogFragment.kt
+++ app/src/main/java/com/nextcloud/ui/ChooseStorageLocationDialogFragment.kt
@@ -18,6 +18,7 @@ import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.nextcloud.client.di.Injectable
 import com.nextcloud.client.preferences.AppPreferencesImpl
+import com.nmc.android.utils.DialogThemeUtils
 import com.owncloud.android.MainApp
 import com.owncloud.android.R
 import com.owncloud.android.databinding.DialogDataStorageLocationBinding
@@ -59,7 +60,8 @@ class ChooseStorageLocationDialogFragment :
                 dialog.dismiss()
             }.setView(binding.root)
 
-        viewThemeUtils.dialog.colorMaterialAlertDialogBackground(requireContext(), builder)
+        // NMC customization: customize dialog bg color
+        DialogThemeUtils.colorMaterialAlertDialogBackground(requireContext(), builder)
 
         binding.storageRadioGroup.setOnCheckedChangeListener { _, _ ->
             updateMediaIndexSwitch()
