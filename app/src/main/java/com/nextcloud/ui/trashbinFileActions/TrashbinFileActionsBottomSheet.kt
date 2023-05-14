63c058a363fed6f4559d8b001a5f66ce2160c6ce
A117870935 <surinder.kumar@t-systems.com>
2023-05-15 01:05:37 +0530

    NMC-2051: Customized bottom sheet. NMC-1929: Collabora related customization with test cases.

--- app/src/main/java/com/nextcloud/ui/trashbinFileActions/TrashbinFileActionsBottomSheet.kt
+++ app/src/main/java/com/nextcloud/ui/trashbinFileActions/TrashbinFileActionsBottomSheet.kt
@@ -12,7 +12,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.IdRes
-import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.os.bundleOf
 import androidx.core.view.isEmpty
 import androidx.core.view.isVisible
@@ -136,7 +136,11 @@ class TrashbinFileActionsBottomSheet :
 
     private fun setMultipleFilesThumbnail() {
         context?.let {
-            val drawable = viewThemeUtils.platform.tintDrawable(it, R.drawable.file_multiple, ColorRole.PRIMARY)
+            // NMC Customization
+            val drawable = viewThemeUtils.platform.colorDrawable(
+                ResourcesCompat.getDrawable(it.resources, R.drawable.file_multiple, null)!!,
+                it.resources.getColor(R.color.primary, null)
+            )
             binding.thumbnailLayout.thumbnail.setImageDrawable(drawable)
         }
     }
@@ -198,12 +202,8 @@ class TrashbinFileActionsBottomSheet :
                 }
                 text.setText(action.title)
                 if (action.icon != null) {
-                    val drawable =
-                        viewThemeUtils.platform.tintDrawable(
-                            requireContext(),
-                            AppCompatResources.getDrawable(requireContext(), action.icon)!!
-                        )
-                    icon.setImageDrawable(drawable)
+                    // NMC customization
+                    icon.setImageResource(action.icon)
                 }
             }
         return itemBinding.root
