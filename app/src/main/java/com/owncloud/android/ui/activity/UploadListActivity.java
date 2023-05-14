eafa7a12a8e59978c6c287c793761834282819bc
A117870935 <surinder.kumar@t-systems.com>
2023-05-14 21:14:03 +0530

    SwipeRefreshLayout Customized.

--- app/src/main/java/com/owncloud/android/ui/activity/UploadListActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/UploadListActivity.java
@@ -31,6 +31,7 @@ import com.nextcloud.client.network.ConnectivityService;
 import com.nextcloud.client.utils.Throttler;
 import com.nextcloud.model.WorkerState;
 import com.nextcloud.model.WorkerStateLiveData;
+import com.nmc.android.utils.SwipeRefreshThemeUtils;
 import com.owncloud.android.R;
 import com.owncloud.android.databinding.UploadListLayoutBinding;
 import com.owncloud.android.datamodel.OCFile;
@@ -174,7 +175,8 @@ public class UploadListActivity extends FileActivity {
         binding.list.setLayoutManager(lm);
         binding.list.setAdapter(uploadListAdapter);
 
-        viewThemeUtils.androidx.themeSwipeRefreshLayout(swipeListRefreshLayout);
+        //NMC Customisation
+        SwipeRefreshThemeUtils.themeSwipeRefreshLayout(this, swipeListRefreshLayout);
         swipeListRefreshLayout.setOnRefreshListener(this::refresh);
 
         loadItems();
