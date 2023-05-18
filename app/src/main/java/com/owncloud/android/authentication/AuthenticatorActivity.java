df4c957084070157ee89676d2dfe53dd53d34d91
A117870935 <surinder.kumar@t-systems.com>
2023-05-18 18:03:56 +0530

    Tealium and Adjust SDKs added from following branches: feature/NMCLOUD-339 feature/NMCLOUD-428

--- app/src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
+++ app/src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
@@ -71,6 +71,8 @@ import com.nextcloud.client.preferences.AppPreferences;
 import com.nextcloud.common.PlainClient;
 import com.nextcloud.operations.PostMethod;
 import com.nextcloud.utils.extensions.BundleExtensionsKt;
+import com.nmc.android.marketTracking.AdjustSdkUtils;
+import com.nmc.android.marketTracking.TealiumSdkUtils;
 import com.owncloud.android.MainApp;
 import com.owncloud.android.R;
 import com.owncloud.android.databinding.AccountSetupBinding;
@@ -1388,6 +1390,10 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
     }
 
     private void endSuccess() {
+        //track successful login event
+        AdjustSdkUtils.trackEvent(AdjustSdkUtils.EVENT_TOKEN_SUCCESSFUL_LOGIN, preferences);
+        TealiumSdkUtils.trackEvent(TealiumSdkUtils.EVENT_SUCCESSFUL_LOGIN, preferences);
+
         if (onlyAdd) {
             finish();
         } else {
@@ -1789,5 +1795,12 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
     @Override
     public void onFailedSavingCertificate() {
         DisplayUtils.showSnackMessage(this, R.string.ssl_validator_not_saved);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //track screen view when activity is visible
+        TealiumSdkUtils.trackView(TealiumSdkUtils.SCREEN_VIEW_LOGIN, preferences);
     }
 }
