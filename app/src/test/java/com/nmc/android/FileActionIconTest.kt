cbe2389743d95204eedb9f4c1a7b3a75d8dff59b
A117870935 <surinder.kumar@t-systems.com>
2023-05-25 17:53:59 +0530

    File action menu icon changed and test case added.

---
+++ app/src/test/java/com/nmc/android/FileActionIconTest.kt
@@ -0,0 +1,16 @@
+package com.nmc.android
+
+import com.nextcloud.ui.fileactions.FileAction
+import org.junit.Assert
+import org.junit.Test
+import com.owncloud.android.R
+
+class FileActionIconTest {
+
+    @Test
+    fun verifyShareAndSyncFileIcon(){
+       val list = FileAction.SORTED_VALUES
+        Assert.assertEquals(R.drawable.ic_share,list[4].icon)
+        Assert.assertEquals(R.drawable.ic_cloud_download,list[15].icon)
+    }
+}
