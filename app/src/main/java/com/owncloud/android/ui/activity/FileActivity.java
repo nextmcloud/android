d01fe67bca31c6ac5a1afc254ae7981eda58b02f
A117870935 <surinder.kumar@t-systems.com>
2023-06-12 15:18:31 +0530

    NMC-2258: Snackbar theme customized. NMC-2047: Customized floating action button.

--- app/src/main/java/com/owncloud/android/ui/activity/FileActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/FileActivity.java
@@ -715,7 +715,6 @@ public abstract class FileActivity extends DrawerActivity
             Snackbar snackbar = Snackbar.make(activity.findViewById(android.R.id.content), R.string.clipboard_text_copied,
                                               Snackbar.LENGTH_LONG)
                 .setAction(R.string.share, v -> showShareLinkDialog(activity, file, link));
-            viewThemeUtils.material.themeSnackbar(snackbar);
             snackbar.show();
         }
     }
@@ -785,7 +784,6 @@ public abstract class FileActivity extends DrawerActivity
                 snackbar = Snackbar.make(sharingFragment.getView(), result.getMessage(), Snackbar.LENGTH_LONG);
             }
 
-            viewThemeUtils.material.themeSnackbar(snackbar);
             snackbar.show();
         }
     }
@@ -862,7 +860,6 @@ public abstract class FileActivity extends DrawerActivity
                                                                                            operation,
                                                                                            getResources()),
                                                   Snackbar.LENGTH_LONG);
-                viewThemeUtils.material.themeSnackbar(snackbar);
                 snackbar.show();
             }
         }
