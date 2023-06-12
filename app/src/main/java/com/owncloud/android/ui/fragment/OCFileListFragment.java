af91016ab4a1475abee20f5d7e0edfef62583159
A117870935 <surinder.kumar@t-systems.com>
2023-06-12 15:18:31 +0530

    NMC-2258: Snackbar theme customized. NMC-2047: Customized floating action button.

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -53,6 +53,7 @@ import com.nextcloud.common.NextcloudClient;
 import com.nextcloud.ui.fileactions.FileAction;
 import com.nextcloud.ui.fileactions.FileActionsBottomSheet;
 import com.nextcloud.utils.EditorUtils;
+import com.nmc.android.utils.FabThemeUtils;
 import com.nextcloud.utils.ShortcutUtil;
 import com.nextcloud.utils.extensions.BundleExtensionsKt;
 import com.nextcloud.utils.extensions.FileExtensionsKt;
@@ -349,7 +350,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
 
         if (mFabMain != null) {
             // is not available in FolderPickerActivity
-            viewThemeUtils.material.themeFAB(mFabMain);
+            //NMC customization
+            FabThemeUtils.colorFloatingActionButton(requireContext(), mFabMain);
         }
 
         Log_OC.i(TAG, "onCreateView() end");
@@ -489,7 +491,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
         }
 
         // is not available in FolderPickerActivity
-        viewThemeUtils.material.themeFAB(mFabMain);
+        //NMC customization
+        FabThemeUtils.colorFloatingActionButton(requireContext(), mFabMain);
         mFabMain.setOnClickListener(v -> {
             var currentDir = getCurrentFile();
             if (currentDir == null) {
@@ -2177,7 +2180,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
         activity.runOnUiThread(() -> {
             if (visible) {
                 mFabMain.show();
-                viewThemeUtils.material.themeFAB(mFabMain);
+                // NMC customization
+                FabThemeUtils.colorFloatingActionButton(requireContext(), mFabMain);
             } else {
                 mFabMain.hide();
             }
@@ -2209,10 +2213,12 @@ public class OCFileListFragment extends ExtendedListFragment implements
             getActivity().runOnUiThread(() -> {
                 if (enabled) {
                     mFabMain.setEnabled(true);
-                    viewThemeUtils.material.themeFAB(mFabMain);
+                    //NMC customization
+                    FabThemeUtils.colorFloatingActionButton(requireContext(), mFabMain);
                 } else {
                     mFabMain.setEnabled(false);
-                    viewThemeUtils.material.themeFAB(mFabMain);
+                    //NMC customization
+                    FabThemeUtils.colorFloatingActionButton(requireContext(), mFabMain);
                 }
             });
         }
