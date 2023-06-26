ef7cad7358f10b0f91bb7875511ecd73152d1f1d
A117870935 <surinder.kumar@t-systems.com>
2023-06-26 23:48:48 +0530

    Download limit functionality added.

--- app/src/main/java/com/owncloud/android/ui/fragment/FileDetailSharingFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/FileDetailSharingFragment.java
@@ -769,6 +769,8 @@ public class FileDetailSharingFragment extends Fragment implements ShareeListAda
                                boolean isExpiryDateShown);
 
         void onShareProcessClosed();
+
+        void onLinkShareDownloadLimitFetched(long downloadLimit, long downloadCount);
     }
 
     @Override
