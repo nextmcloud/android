c7bf7fa4fd7c00b2906f1b4d22ac992e5fa782bd
A117870935 <surinder.kumar@t-systems.com>
2023-09-12 18:09:27 +0530

    Comment functionality customized as per NMC.

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -645,7 +645,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
     @Override
     public void onShareIconClick(OCFile file) {
         if (file.isFolder()) {
-            mContainerActivity.showDetails(file, 1);
+            // NMC: use 0 as activeTab
+            mContainerActivity.showDetails(file, 0);
         } else {
             throttler.run("shareIconClick", () -> {
                 mContainerActivity.getFileOperationsHelper().sendShareFile(file);
@@ -660,7 +661,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
 
     @Override
     public void showActivityDetailView(OCFile file) {
-        mContainerActivity.showDetails(file, 0);
+        // NMC: use 1 as activeTab
+        mContainerActivity.showDetails(file, 1);
     }
 
     @Override
