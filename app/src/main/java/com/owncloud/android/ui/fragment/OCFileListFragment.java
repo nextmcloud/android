f962bbbaa6ffd7f90057a4222d9654666050ed2b
A117870935 <surinder.kumar@t-systems.com>
2023-09-12 18:09:27 +0530

    Comment functionality customized as per NMC.

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -496,7 +496,7 @@ public class OCFileListFragment extends ExtendedListFragment implements
                 Log_OC.w(TAG, "currentDir is null cannot open bottom sheet dialog");
                 return;
             }
-            
+
             final OCFileListBottomSheetDialog dialog = new OCFileListBottomSheetDialog(fileActivity,
                                                                                        this,
                                                                                        deviceInfo,
@@ -618,7 +618,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
 
     @Override
     public void onShareIconClick(OCFile file) {
-        mContainerActivity.showDetails(file, 1);
+        // NMC: use 0 as activeTab
+        mContainerActivity.showDetails(file, 0);
     }
 
     @Override
@@ -628,7 +629,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
 
     @Override
     public void showActivityDetailView(OCFile file) {
-        mContainerActivity.showDetails(file, 0);
+        // NMC: use 1 as activeTab
+        mContainerActivity.showDetails(file, 1);
     }
 
     @Override
@@ -1583,7 +1585,7 @@ public class OCFileListFragment extends ExtendedListFragment implements
 
         invalidateActionMode();
     }
-    
+
     private void updateSortButton() {
         if (mSortButton != null) {
             FileSortOrder sortOrder;
