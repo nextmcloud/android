e3fbc4d7449a30fd261b863b5cda136f8da398cf
A117870935 <surinder.kumar@t-systems.com>
2023-10-18 13:05:48 +0530

    In-App update implemented with Remote config.

---
+++ app/src/generic/java/com/nmc.android.appupdate/InAppUpdateHelperImpl.kt
@@ -0,0 +1,12 @@
+package com.nmc.android.appupdate
+
+import androidx.appcompat.app.AppCompatActivity
+
+class InAppUpdateHelperImpl(private val activity: AppCompatActivity) : InAppUpdateHelper {
+
+    override fun onResume() {
+    }
+
+    override fun onDestroy() {
+    }
+}
