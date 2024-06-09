e171b447391fe2385f2627c7c9f741d4f47b3e90
A117870935 <surinder.kumar@t-systems.com>
2024-06-09 23:12:28 +0530

    MoEngage functionality

--- app/src/main/java/com/owncloud/android/ui/activity/SyncedFoldersActivity.kt
+++ app/src/main/java/com/owncloud/android/ui/activity/SyncedFoldersActivity.kt
@@ -39,6 +39,7 @@ import com.nextcloud.client.preferences.SubFolderRule
 import com.nextcloud.utils.extensions.getParcelableArgument
 import com.nextcloud.utils.extensions.isDialogFragmentReady
 import com.nextcloud.utils.extensions.setVisibleIf
+import com.nmc.android.marketTracking.MoEngageSdkUtils
 import com.owncloud.android.BuildConfig
 import com.owncloud.android.MainApp
 import com.owncloud.android.R
@@ -712,6 +713,7 @@ class SyncedFoldersActivity :
         if (syncedFolder.isEnabled) {
             showBatteryOptimizationInfo()
         }
+        trackAutoUpload()
     }
 
     override fun showSubFolderWarningDialog() {
@@ -774,6 +776,15 @@ class SyncedFoldersActivity :
 
         syncedFolderProvider.deleteSyncedFolder(syncedFolder.id)
         adapter.removeItem(syncedFolder.section)
+        trackAutoUpload()
+    }
+
+    /**
+     * NMC: tracking auto upload is enabled or not
+     * Should be called whenever a Folder is saved or removed from auto upload
+     */
+    private fun trackAutoUpload() {
+        MoEngageSdkUtils.trackAutoUpload(this, syncedFolderProvider.countEnabledSyncedFolders())
     }
 
     /**
