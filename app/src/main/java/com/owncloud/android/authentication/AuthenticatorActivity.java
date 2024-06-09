fdc6cd70c1a95baa04481074e9d41820c012230d
A117870935 <surinder.kumar@t-systems.com>
2024-06-09 23:12:28 +0530

    MoEngage functionality

--- app/src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
+++ app/src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
@@ -65,6 +65,7 @@ import com.nextcloud.operations.PostMethod;
 import com.nextcloud.utils.extensions.BundleExtensionsKt;
 import com.nextcloud.utils.mdm.MDMConfig;
 import com.owncloud.android.MainApp;
+import com.nmc.android.marketTracking.MoEngageSdkUtils;
 import com.owncloud.android.R;
 import com.owncloud.android.databinding.AccountSetupBinding;
 import com.owncloud.android.databinding.AccountSetupWebviewBinding;
@@ -1278,6 +1279,8 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
             if (success) {
                 accountManager.setCurrentOwnCloudAccount(mAccount.name);
                 getUserCapabilitiesAndFinish();
+                // NMC: MoEngage user login event tracking
+                trackUserLoginEvent(result.getResultData());
             } else {
                 accountSetupBinding = AccountSetupBinding.inflate(getLayoutInflater());
                 setContentView(accountSetupBinding.getRoot());
@@ -1320,6 +1323,10 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
 
             Log_OC.d(TAG, "Access failed: " + result.getLogMessage());
         }
+    }
+
+    private void trackUserLoginEvent(UserInfo userInfo) {
+        MoEngageSdkUtils.trackUserLogin(this, userInfo);
     }
 
     private void endSuccess() {
--- app/src/main/java/com/owncloud/android/ui/fragment/contactsbackup/BackupFragment.kt
+++ app/src/main/java/com/owncloud/android/ui/fragment/contactsbackup/BackupFragment.kt
@@ -28,6 +28,7 @@ import com.nextcloud.client.di.Injectable
 import com.nextcloud.client.jobs.BackgroundJobManager
 import com.nextcloud.utils.extensions.getSerializableArgument
 import com.nextcloud.utils.extensions.setVisibleIf
+import com.nmc.android.marketTracking.MoEngageSdkUtils
 import com.owncloud.android.R
 import com.owncloud.android.databinding.BackupFragmentBinding
 import com.owncloud.android.datamodel.ArbitraryDataProvider
@@ -96,6 +97,8 @@ class BackupFragment : FileFragment(), OnDateSetListener, Injectable {
                 PREFERENCE_CONTACTS_BACKUP_ENABLED,
                 enabled
             )
+            // NMC: track contact backup
+            MoEngageSdkUtils.trackContactBackup(requireContext(), enabled)
         }
 
     private lateinit var contactsBackupFolderPath: String
