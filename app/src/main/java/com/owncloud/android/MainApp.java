9d454f172ddef93bbf2baf5519cc0f7c007cf953
A117870935 <surinder.kumar@t-systems.com>
2024-06-09 23:12:28 +0530

    MoEngage functionality

--- app/src/main/java/com/owncloud/android/MainApp.java
+++ app/src/main/java/com/owncloud/android/MainApp.java
@@ -61,6 +61,7 @@ import com.nextcloud.client.preferences.DarkMode;
 import com.nextcloud.ui.composeActivity.ComposeProcessTextAlias;
 import com.nextcloud.utils.extensions.ContextExtensionsKt;
 import com.nextcloud.utils.mdm.MDMConfig;
+import com.nmc.android.marketTracking.MoEngageSdkUtils;
 import com.nmc.android.ui.LauncherActivity;
 import com.owncloud.android.authentication.PassCodeManager;
 import com.owncloud.android.datamodel.ArbitraryDataProvider;
@@ -293,6 +294,9 @@ public class MainApp extends Application implements HasAndroidInjector, NetworkC
 
         registerActivityLifecycleCallbacks(new ActivityInjector());
 
+        // NMC: init MoEngage SDK
+        initMoEngage();
+        // NMC: end
         //update the app restart count when app is launched by the user
         inAppReviewHelper.resetAndIncrementAppRestartCounter();
 
@@ -994,6 +998,13 @@ public class MainApp extends Application implements HasAndroidInjector, NetworkC
                 preferences.setLegacyClean(true);
             }
         }
+    }
+
+    private void initMoEngage(){
+        MoEngageSdkUtils.initMoEngageSDK(this);
+        MoEngageSdkUtils.trackAppInstallOrUpdate(this, preferences.getLastSeenVersionCode());
+        MoEngageSdkUtils.captureUserAttrsForOldAppVersion(this, preferences.getLastSeenVersionCode(),
+                                                          accountManager.getUser());
     }
 
     @Override
