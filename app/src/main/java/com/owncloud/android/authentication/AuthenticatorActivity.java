7bef591e45b4c336a4a038beda2211e92c8ab170
A117870935 <surinder.kumar@t-systems.com>
2024-06-12 22:33:57 +0530

    Added User Attributes tracking.

--- app/src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
+++ app/src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
@@ -67,6 +67,7 @@ import com.nextcloud.operations.PostMethod;
 import com.nextcloud.utils.extensions.BundleExtensionsKt;
 import com.nextcloud.utils.mdm.MDMConfig;
 import com.owncloud.android.MainApp;
+import com.nmc.android.marketTracking.MoEngageSdkUtils;
 import com.owncloud.android.R;
 import com.owncloud.android.databinding.AccountSetupBinding;
 import com.owncloud.android.databinding.AccountSetupWebviewBinding;
@@ -1216,6 +1217,8 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
             if (success) {
                 accountManager.setCurrentOwnCloudAccount(mAccount.name);
                 getUserCapabilitiesAndFinish();
+                // NMC: MoEngage user login event tracking
+                trackUserLoginEvent(result.getResultData());
             } else {
                 accountSetupBinding = AccountSetupBinding.inflate(getLayoutInflater());
                 setContentView(accountSetupBinding.getRoot());
@@ -1258,6 +1261,10 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
 
             Log_OC.d(TAG, "Access failed: " + result.getLogMessage());
         }
+    }
+
+    private void trackUserLoginEvent(UserInfo userInfo) {
+        MoEngageSdkUtils.trackUserLogin(this, userInfo);
     }
 
     private void endSuccess() {
