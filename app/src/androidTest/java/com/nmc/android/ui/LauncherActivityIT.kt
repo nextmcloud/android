ed008f25c85d305e048b21bd998dc99f052b57e6
A117870935 <surinder.kumar@t-systems.com>
2024-08-12 18:31:18 +0530

    NMC-3250 -> opening login flow in internal webview instead of external browser. NMC-1885: Splash screen customized NMC-571 -- removed usesCleartextTraffic from Manifest.

--- app/src/androidTest/java/com/nmc/android/ui/LauncherActivityIT.kt
+++ app/src/androidTest/java/com/nmc/android/ui/LauncherActivityIT.kt
@@ -6,72 +6,33 @@
  */
 package com.nmc.android.ui
 
-import androidx.annotation.UiThread
-import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.owncloud.android.AbstractIT
 import com.owncloud.android.R
-import com.owncloud.android.utils.EspressoIdlingResource
-import org.junit.After
-import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LauncherActivityIT : AbstractIT() {
 
-    @Before
-    fun registerIdlingResource() {
-        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
-    }
-
-    @After
-    fun unregisterIdlingResource() {
-        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
-    }
+    @get:Rule
+    val activityRule = ActivityScenarioRule(LauncherActivity::class.java)
 
     @Test
-    @UiThread
-    fun testSplashScreenWithEmptyTitlesShouldHideTitles() {
-        launchActivity<LauncherActivity>().use { scenario ->
-            scenario.onActivity { _ ->
-                onIdleSync {
-                    onView(withId(R.id.ivSplash)).check(matches(isCompletelyDisplayed()))
-                    onView(
-                        withId(R.id.splashScreenBold)
-                    ).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
-                    onView(
-                        withId(R.id.splashScreenNormal)
-                    ).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
-                }
-            }
-        }
-    }
+    fun verifyUIElements() {
+        onView(withId(R.id.ivSplash)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.splashScreenBold)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.splashScreenNormal)).check(matches(isCompletelyDisplayed()))
 
-    @Test
-    @UiThread
-    fun testSplashScreenWithTitlesShouldShowTitles() {
-        launchActivity<LauncherActivity>().use { scenario ->
-            scenario.onActivity {
-                onIdleSync {
-                    onView(withId(R.id.ivSplash)).check(matches(isCompletelyDisplayed()))
-
-                    EspressoIdlingResource.increment()
-                    it.setSplashTitles("Example", "Cloud")
-                    EspressoIdlingResource.decrement()
-
-                    val onePercentArea = ViewMatchers.isDisplayingAtLeast(1)
-                    onView(withId(R.id.splashScreenBold)).check(matches(onePercentArea))
-                    onView(withId(R.id.splashScreenNormal)).check(matches(onePercentArea))
-                }
-            }
-        }
+        onView(withId(R.id.splashScreenBold)).check(matches(withText("Magenta")))
+        onView(withId(R.id.splashScreenNormal)).check(matches(withText("CLOUD")))
+        shortSleep()
     }
 }
